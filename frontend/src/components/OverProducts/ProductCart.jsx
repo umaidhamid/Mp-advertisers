7f059955cc0cc9012ea0ce9123416102f7fe72b1
Umaid Hamid <umaid.uk39@gmail.com>
2025-11-02 22:49:18 +0530

    prefinal changes 1

--- frontend/src/components/OverProducts/ProductCart.jsx
+++ frontend/src/components/OverProducts/ProductCart.jsx
@@ -53,10 +53,10 @@ const ProductCart = ({ product }) => {
         </h3>
         <h1 className="Product-unit">{product.unit}</h1>
       </div>
-      {dis && <p className="product-description">{product.descriptionn }loremkjansdkjaskjdkjasdkjnaskdjnaskdjnaskjdnaskjdnaskjdnaskjdnaskjnk</p>}
+      {dis && <p className="product-description">{product.descriptionn }</p>}
       <div style={{ display: "flex ", width: "100%", gap: "10px" }}>
         <p className="discription" onClick={() => setdis(!dis)}>
-          discription
+         Detail
         </p>
 
         <button className="cart">Add to Cart</button>
