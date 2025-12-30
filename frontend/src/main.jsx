5c993b7b8b48df313ac12127d6add70a71f340fc
Umaid Hamid <umaid.oasis@gmail.com>
2025-12-30 21:44:40 +0530

    checked

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 import AppRoute from "./Routes/AppRoute.jsx";
-import { AuthProvider } from "./Context/AuthContext";
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <AppRoute />
